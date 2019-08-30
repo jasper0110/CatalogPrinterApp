db48d998fed93bbfe142fcf823683f8b01e67741
jasper <jasper.lammens@hotmail.com>
2019-08-30 12:43:26 +0200

    Add Worksheet extension methods

---
+++ ExcelUtil/WorksheetExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelUtil
+{
+    public static class WorksheetExtensions
+    {
+        public static bool IsDrawingTarief(this Worksheet ws)
+        {
+            var name = ws.Name;
+            int.TryParse(name, out int i);
+            return i > 500 && i < 1000;
+        }
+
+        public static bool IsCoverTarief(this Worksheet ws)
+        {
+            var name = ws.Name;
+            int.TryParse(name, out int i);
+            return i > 1000 && i != 1970 && i != 1980 && i != 1990;
+        }
+    }
+}
